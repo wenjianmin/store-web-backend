a4ccbc9a20b9eac5e4363fb58ebdc0510e6c2009
jmin <1256527506@qq.com>
2024-10-14 09:05:32 +0800

    add: excelService

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -6,18 +6,25 @@ import {
   Patch,
   Param,
   Query,
+  UseInterceptors,
+  UploadedFile,
+  Res,
 } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { ProductListDto } from './dto/product-list.dto';
 import { HotSalesService } from './hot-sales.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { ExcelService } from 'src/common/excel/excel.service';
+import { Response } from 'express';
 
 @Controller('product')
 export class ProductController {
   constructor(
     private readonly productService: ProductService,
     private readonly hotSalesService: HotSalesService,
+    private readonly excelService: ExcelService,
   ) {}
 
   @Post('create')
@@ -52,4 +59,39 @@ export class ProductController {
       updateProductDto.status,
     );
   }
+  @Post('import')
+  @UseInterceptors(FileInterceptor('file'))
+  async importProducts(@UploadedFile() file: Express.Multer.File) {
+    const data = await this.excelService.importExcel(file);
+    console.log(data);
+    return {
+      message: '上传成功',
+      file: file.filename,
+      data
+    };
+  }
+
+  @Post('export')
+  async exportProducts(@Res() res: Response) {
+    // 定义列
+    const columns = [
+      { header: 'ID', key: 'id', width: 10 },
+      { header: 'Name', key: 'name', width: 32 },
+      { header: 'Email', key: 'email', width: 32 },
+    ];
+
+    // 查询数据库或获取数据
+    const data = [
+      { id: 1, name: 'John Doe', email: 'john@example.com' },
+      { id: 2, name: 'Jane Doe', email: 'jane@example.com' },
+    ];
+    // 导出数据到Excel
+    const buffer = await this.excelService.exportExcel(columns, data, '产品');
+
+    res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+    res.setHeader('Content-Disposition', 'attachment; filename="users.xlsx"');
+
+    return res.send(buffer);
+  }
+  
 }
--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -4,11 +4,12 @@ import { ProductController } from './product.controller';
 import { ProductEntity } from './entities/product.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { HotSalesService } from './hot-sales.service';
+import { ExcelService } from 'src/common/excel/excel.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([ProductEntity])],
   controllers: [ProductController],
-  providers: [ProductService, HotSalesService],
+  providers: [ProductService, HotSalesService, ExcelService],
   exports: [HotSalesService]
 })
 export class ProductModule {}
